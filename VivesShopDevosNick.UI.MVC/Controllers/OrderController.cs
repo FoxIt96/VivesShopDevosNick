8aedc91dd692e5a61bb7b6c0176ec285e90327f3
Nick Devos <145980327+FoxIt96@users.noreply.github.com>
2023-11-12 12:42:17 +0100

    final 12/11/23

--- VivesShopDevosNick.UI.MVC/Controllers/OrderController.cs
+++ VivesShopDevosNick.UI.MVC/Controllers/OrderController.cs
@@ -1,43 +1,45 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using VivesShopDevosNick.UI.MVC.Models;
 
-using Microsoft.AspNetCore.Mvc;
-using VivesShopDevosNick.UI.MVC.Models;
-namespace VivesShopDevosNick.UI.MVC.Controllers
+
+public class OrderController : Controller
 {
-    public class OrderController : Controller
+    private IOrderRepository repository;
+    private Cart cart;
+
+    public OrderController(IOrderRepository repoService, Cart cartService)
     {
-        private IOrderRepository repository;
-        private Cart cart;
+        repository = repoService;
+        cart = cartService;
+    }
+    [HttpGet]
+    public ViewResult Checkout()
+    {
+        var order = new Order();
+        order.Items = cart.Items.ToList();
+        return View(order);
+    }
 
-        public OrderController(IOrderRepository repoService, Cart cartService)
+    [HttpPost]
+    public IActionResult Checkout(Order order)
+    {
+        if (cart.Items.Count() == 0)
         {
-            repository=repoService;
-            cart=cartService;
+            ModelState.AddModelError("", "Sorry, your cart is empty");
         }
 
-        public ViewResult Checkout() => View(new Order());
-
-        [HttpPost]
-        public IActionResult Checkout(Order order)
+        if (ModelState.IsValid)
         {
-            if (cart.Items.Count() == 0)
-            {
-                ModelState.AddModelError("","sorry, your cart is empty");
-            }
-
-            if (ModelState.IsValid)
-            {
-                order.Items = cart.Items.ToArray();
-                repository.SaveOrder(order);
-                cart.Clear();
-                return RedirectToPage("/Completed", new { orderId = order.OrderID });
-            }
-            else
-            {
-                return View();
-            }
+            order.Items = cart.Items.ToArray();
+            order.PaymentMethod = order.PaymentMethod;
+            repository.SaveOrder(order);
+            cart.Clear();
+            return View("Completed", order);
         }
-
+        else
+        {
+            order.Items = cart.Items.ToList();
+            return View(order);
+        }
     }
 }
--- VivesShopDevosNick.UI.MVC/Models/Order.cs
+++ VivesShopDevosNick.UI.MVC/Models/Order.cs
@@ -1,46 +1,45 @@
-﻿
+﻿using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
-
 
 namespace VivesShopDevosNick.UI.MVC.Models
 {
     [Table(nameof(Order))]
     public class Order
-	{
+    {
+        [BindNever]
+        public int OrderID { get; set; }
+        [BindNever]
+        public ICollection<CartItem> Items { get; set; } = new List<CartItem>();
 
-            
-            public int OrderID { get; set; }
-            public ICollection<CartItem> Items { get; set; } = new List<CartItem>();
+        [Required]
+        [Display(Name = "Name")]
+        public string? Name { get; set; }
 
-            [Required]
-            [Display(Name = "Name" )]
-			public string? Name { get; set; }
+        [Required]
+        [Display(Name = "Email")]
+        public string? Email { get; set; }
 
-            [Required]
-            [Display(Name = "Email")]
-            public string? Email { get; set; }
+        [Required]
+        [Display(Name = "Address")]
+        public string? Address { get; set; }
 
-            [Required]
-            [Display(Name = "Address")]
-            public string? Address { get; set; }
-            
-            [Required]
-            [Display(Name = "Zipcode")]
-            public string? Zip { get; set; }
+        [Required]
+        [Display(Name = "Zipcode")]
+        public string? Zip { get; set; }
 
-            [Required]
-            [Display(Name = "City")]
-			public string? City { get; set;}
+        [Required]
+        [Display(Name = "City")]
+        public string? City { get; set; }
 
-            [Required]
-            [Display(Name = "Country")]
-            public string? Country { get; set; }
+        [Required]
+        [Display(Name = "Country")]
+        public string? Country { get; set; }
 
-        
+        [Required]
+        [Display(Name = "Payment Method")]
+        public string? PaymentMethod { get; set; }
 
     }
-	
 }
-
